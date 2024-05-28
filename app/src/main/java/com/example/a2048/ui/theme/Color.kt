a14ae59143a92f941ad340afcf102e1c6cc9fff0
PaulaSanchez <pausanchezt12@gmail.com>
2024-05-27 23:28:00 -0500

    designing view game

---
+++ app/src/main/java/com/example/a2048/ui/theme/Color.kt
@@ -0,0 +1,28 @@
+package com.example.a2048.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object Colors {
+    val background = Color(0XFFF2EBFB)
+    val board = Color(0XFFD4BBFC)
+
+    val numberColors = mapOf(
+        0 to Color(0XFFEBD9FC),
+        2 to Color(0XFFFFCBF2),
+        4 to Color(0XFFD8BBFF),
+        8 to Color(0XFFD0D1FF),
+        16 to Color(0XFFBEE1E6),
+        32 to Color(0XFFE2ECE9),
+        64 to Color(0XFFDFE7FD),
+        128 to Color(0XFFE0AAFF),
+        256 to Color(0XFFC77DFF),
+        512 to Color(0XFF957FEF),
+        1024 to Color(0XFFABC4FF),
+        2048 to Color(0XFFCCDBFD),
+        4096 to Color(0XFFb8c0ff),
+        8192 to Color(0XFF9c89b8),
+        16384 to Color(0XFFbe95c4),
+    )
+
+    val title = Color(0XFF9667E0)
+}
---
+++ app/src/main/java/com/example/a2048/ui/theme/Theme.kt
@@ -0,0 +1,33 @@
+package com.example.a2048.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+private val DarkColorPalette = darkColorScheme()
+
+private val LightColorPalette = lightColorScheme()
+
+@Composable
+fun _2048Theme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
